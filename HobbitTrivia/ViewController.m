4234c291a08cc2177a872eb53a8df3a3be701748
Jim Rutherford <jim.rutherford@gmail.com>
2013-01-12 10:57:24 -0800

    A bit of refactoring and adding some comments

--- HobbitTrivia/ViewController.m
+++ HobbitTrivia/ViewController.m
@@ -29,8 +29,8 @@ bool isSlowing = NO;
 bool didMakeChoice = NO;
 
 NSTimer *wheelTimer;
+static float angle = 0;
 float angleIncrement;
-float totalAngle;
 int correctChoice;
 
 @synthesize questionView;
@@ -69,18 +69,13 @@ int correctChoice;
 #pragma mark - Animations
 
 - (void)move:(id)timer {
-
-	static float angle = 0;
 	self.WheelImage.transform = CGAffineTransformMakeRotation(DEGREES_TO_RADIANS(angle));
     
 	angle += angleIncrement;
-	totalAngle += angleIncrement;
-	
-	if (totalAngle > 360)
-	{
-		totalAngle = totalAngle - 360;
-	}
 
+	// we never need our angle to be larger than 360 - adjust it
+	if (angle > 360) angle = angle - 360;
+	
 	if (angleIncrement < 1)
 	{
 		[wheelTimer invalidate];
@@ -103,7 +98,6 @@ int correctChoice;
 
 - (void) showQuestion
 {
-	
 	didMakeChoice = NO;
 	
 	NSString *question;
@@ -112,7 +106,7 @@ int correctChoice;
 	NSString *answer3;
 	NSString *answer4;
 	
-	if (totalAngle > 0.0f && totalAngle < 36.0f)
+	if (angle > 0.0f && angle < 36.0f)
 	{
 		question = @"What is another name for the Heart of the Mountain?";
 		answer1 = @"Answer_1";
@@ -122,7 +116,7 @@ int correctChoice;
 		correctChoice = 1;
 	}
 	
-	if (totalAngle > 36.0f && totalAngle < 72.0f)
+	if (angle > 36.0f && angle < 72.0f)
 	{
 		question = @"What is the name of the Brown Wizard?";
 		answer1 = @"Answer_1";
@@ -132,7 +126,7 @@ int correctChoice;
 		correctChoice = 1;
 	}
 	
-	if (totalAngle > 72.0f && totalAngle < 108.0f)
+	if (angle > 72.0f && angle < 108.0f)
 	{
 		question = @"Sauron";
 		answer1 = @"Answer_1";
@@ -142,7 +136,7 @@ int correctChoice;
 		correctChoice = 1;
 	}
 	
-	if (totalAngle > 108.0f && totalAngle < 144.0f)
+	if (angle > 108.0f && angle < 144.0f)
 	{
 		question = @"Who made the Ring to Rule them All?";
 		answer1 = @"Answer_1";
@@ -152,7 +146,7 @@ int correctChoice;
 		correctChoice = 1;
 	}
 	
-	if (totalAngle > 144.0f && totalAngle < 180.0f)
+	if (angle > 144.0f && angle < 180.0f)
 	{
 		question = @"What is the name of the Queen of the Elves?";
 		answer1 = @"Answer_1";
@@ -162,7 +156,7 @@ int correctChoice;
 		correctChoice = 1;
 	}
 	
-	if (totalAngle > 180.0f && totalAngle < 216.0f)
+	if (angle > 180.0f && angle < 216.0f)
 	{
 		question = @"Thorin";
 		answer1 = @"Answer_1";
@@ -172,7 +166,7 @@ int correctChoice;
 		correctChoice = 1;
 	}
 	
-	if (totalAngle > 216.0f && totalAngle < 252.0f)
+	if (angle > 216.0f && angle < 252.0f)
 	{
 		question = @"Smaug";
 		answer1 = @"Answer_1";
@@ -182,7 +176,7 @@ int correctChoice;
 		correctChoice = 1;
 	}
 	
-	if (totalAngle > 252.0f && totalAngle < 288.0f)
+	if (angle > 252.0f && angle < 288.0f)
 	{
 		question = @"What are the names of the swords...?";
 		answer1 = @"Answer_1";
@@ -192,7 +186,7 @@ int correctChoice;
 		correctChoice = 1;
 	}
 	
-	if (totalAngle > 288.0f && totalAngle < 324.0f)
+	if (angle > 288.0f && angle < 324.0f)
 	{
 		question = @"What is Gollums real name?";
 		answer1 = @"Answer_1";
@@ -203,7 +197,7 @@ int correctChoice;
 		
 	}
 	
-	if (totalAngle > 324.0f && totalAngle < 360.0f)
+	if (angle > 324.0f && angle < 360.0f)
 	{
 		question = @"What is the name of the home of the Dwarves?";
 		answer1 = @"Answer_1";
@@ -216,16 +210,19 @@ int correctChoice;
 	
 	[self.questionLabel setText:question];
 	
+	// set all of our answers to black text
 	[self.answerLabel1 setTextColor:[UIColor blackColor]];
 	[self.answerLabel2 setTextColor:[UIColor blackColor]];
 	[self.answerLabel3 setTextColor:[UIColor blackColor]];
 	[self.answerLabel4 setTextColor:[UIColor blackColor]];
 	
+	// set the answer label text
 	[self.answerLabel1 setText:answer1];
 	[self.answerLabel2 setText:answer2];
 	[self.answerLabel3 setText:answer3];
 	[self.answerLabel4 setText:answer4];
 	
+	// setup our initial animation state - make the view a bit bigger so we can zoom it down
 	questionView.transform = CGAffineTransformMakeScale(1.25, 1.25);
 	
 	[UIView animateWithDuration:0.25 animations:^{
@@ -233,6 +230,7 @@ int correctChoice;
         questionView.transform = CGAffineTransformIdentity;
     }];
 	
+	// get rid of the spin buton
 	[UIView animateWithDuration:0.25 animations:^{
         self.SpinButton.alpha = 0;
     }];
@@ -252,10 +250,12 @@ int correctChoice;
 			int selectedAnswer = touch.view.tag - 100;
 			UILabel *label = (UILabel*)touch.view;
 			
+			// set the frame position of the background image for feedback on correct/incorrect answer.  Offset it by 6 pixels below the top of the answer label.
 			CGRect answerFrame = CGRectMake(self.answerBG.frame.origin.x, label.frame.origin.y + 6, self.answerBG.frame.size.width, self.answerBG.frame.size.height);
 			
 			self.answerBG.frame = answerFrame;
 			
+			// set the background image based on if our choice is correct
 			if (selectedAnswer == correctChoice)
 			{
 				[self.answerBG setImage:[UIImage imageNamed:@"CorrectAnswerBG"]];
@@ -263,6 +263,7 @@ int correctChoice;
 				[self.answerBG setImage:[UIImage imageNamed:@"WrongAnswerBG"]];
 			}
 			
+			// lets animate the background in
 			[UIView animateWithDuration:0.25 animations:^{
 				self.answerBG.alpha = 1;
 				label.textColor = [UIColor whiteColor];
